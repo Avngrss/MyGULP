ee7d929de9706efd119ccce4eaeb9b57e5c9ab8a
Yurii Sedow <sedow94@mail.ru>
2022-09-13 22:32:45 +0300

    add pug file

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,37 +1,48 @@
 import fileInclude from "gulp-file-include";
 import webphtmlNosvg from "gulp-webp-html-nosvg";
 import versionNumber from "gulp-version-number";
+import pug from "gulp-pug";
 
 export const html = () => {
-  return app.gulp
-    .src(app.path.src.html)
-    .pipe(
-      app.plugins.plumber(
-        app.plugins.notify.onError({
-          title: "HTML",
-          message: "Error: <%= error.message %>",
+  return (
+    app.gulp
+      .src(app.path.src.html)
+      .pipe(
+        app.plugins.plumber(
+          app.plugins.notify.onError({
+            title: "HTML",
+            message: "Error: <%= error.message %>",
+          })
+        )
+      )
+      // .pipe(fileInclude())
+      .pipe(
+        pug({
+          //Сжатие HTML файла
+          pretty: true,
+          // Показать в терминале какой файл обработан
+          verbose: true,
         })
       )
-    )
-    .pipe(fileInclude())
-    .pipe(app.plugins.replace(/@img\//g, "img/"))
-    .pipe(app.plugins.if(app.isBuild, webphtmlNosvg()))
-    .pipe(
-      app.plugins.if(
-        app.isBuild,
-        versionNumber({
-          value: "%DT%",
-          append: {
-            key: "_v",
-            cover: 0,
-            to: ["css", "js"],
-          },
-          output: {
-            file: "gulp/version.json",
-          },
-        })
+      .pipe(app.plugins.replace(/@img\//g, "img/"))
+      .pipe(app.plugins.if(app.isBuild, webphtmlNosvg()))
+      .pipe(
+        app.plugins.if(
+          app.isBuild,
+          versionNumber({
+            value: "%DT%",
+            append: {
+              key: "_v",
+              cover: 0,
+              to: ["css", "js"],
+            },
+            output: {
+              file: "gulp/version.json",
+            },
+          })
+        )
       )
-    )
-    .pipe(app.gulp.dest(app.path.build.html))
-    .pipe(app.plugins.browsersync.stream());
+      .pipe(app.gulp.dest(app.path.build.html))
+      .pipe(app.plugins.browsersync.stream())
+  );
 };
