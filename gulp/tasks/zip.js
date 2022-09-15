4da23ac3b2b52bed64ad64f4f2b782d9dd080590
Yurii Sedow <sedow94@mail.ru>
2022-09-15 23:14:38 +0300

    fix for zip dist

--- gulp/tasks/zip.js
+++ gulp/tasks/zip.js
@@ -4,7 +4,7 @@ import zipPlugin from "gulp-zip";
 export const zip = () => {
   del(`./${app.path.rootFolder}.zip`);
   return app.gulp
-    .src(`${app.path.buildFolder}/**/*.*`, {})
+    .src("./dist/**/*.*", {})
     .pipe(
       app.plugins.plumber(
         app.plugins.notify.onError({
